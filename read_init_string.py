709de53ccd048c98d3738c1c87b691cc4b7817c8
sspatari <spatari.stanislav@gmail.com>
2016-12-06 20:37:34 +0200

    created base_string.json and grammar.json file, removed productions.txt and base_string.txt, changed read_init_string.py to read_strin_and_grammar.py

--- read_init_string.py
+++
@@ -1,7 +0,0 @@
-#Returns the read base_string
-def read_base_string():
-    #Open initial_string.txt for reading and read first line
-    fo = open("base_string.txt","r")
-    base_string = fo.readline().strip() #removes white spaces and splits
-    fo.close()
-    return base_string
---
+++ read_string_and_grammar.py
@@ -0,0 +1,14 @@
+import json
+
+#Returns the read base_string
+def read_json_file(file_name):
+    fo = open(file_name,"r") #Open base_string.json for reading
+    data_string = fo.read() #obtain string from file
+    data = json.loads(data_string) #from string to data
+    fo.close()
+    return data;
+
+string_json = read_json_file("base_string.json")
+grammar_json = read_json_file("grammar.json")
+#print(string_json)
+#print(grammar_json)
