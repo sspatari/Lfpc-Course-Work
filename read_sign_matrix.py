0ae8fff92bfee2a7076975c863a99340f4c3a000
strdr4605 <str.dr4605@gmail.com>
2016-12-07 00:45:27 +0200

    Create get_final_string and check_final_string

--- read_sign_matrix.py
+++ read_sign_matrix.py
@@ -1,11 +1,51 @@
 import json
+from read_string_and_grammar import *
 from pprint import pprint
 
-
-def getSignMatrixObj():
+def get_sign_matrix_obj():
     with open('sign_matrix.json') as data_file:
         return json.load(data_file)
 
-data = getSignMatrixObj()
+def get_final_string(before_str):
+    after_str = before_str.replace('$<', '$').replace('>$', '$').replace('$', '>$<')
+    after_str = after_str[1:len(after_str) - 1:]
+    for rule in data['matrix']:
+        before = rule['x1'] + rule['x2']
+        after = rule['x1'] + rule['sign'] + rule['x2']
+        after_str = after_str.replace(before, after)
+    return after_str
 
-pprint(data)
+def replace_string(final_string, productions):
+    end = final_string.index('>')
+    cut_string = final_string[0:end + 1:1][::-1]
+    begin = cut_string.index('<')
+    selected_substring = cut_string[0:begin + 1:1][::-1]
+    productions_value = selected_substring.replace('=', '')
+    replace_value = ''
+    for item in productions:
+        for key, value in item.items():
+            if '<' + value + '>' == productions_value:
+                replace_value = key
+    final_string = final_string.replace(selected_substring, replace_value)
+    final_string = get_final_string(final_string)
+    return final_string.replace('<>', '')
+
+
+def check_final_string(final_string, productions):
+    while final_string != '$$':
+        final_string = replace_string(final_string, productions)
+        if final_string == '$<' + get_start() + '>$':
+            return True
+    return False
+
+data = get_sign_matrix_obj()
+string = get_string()
+final_string = get_final_string('$' + string + '$')
+productions = get_productions()
+
+# replace_string('d<i=A>v<i<a<a', productions)
+# pprint(data)
+# pprint(string)
+# pprint(final_string)
+# pprint(productions)
+pprint(check_final_string(final_string, productions))
