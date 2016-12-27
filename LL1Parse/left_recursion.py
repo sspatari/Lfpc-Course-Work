057c455a3c85fed74005f8cb5af65adcb2a63f35
sspatari <spatari.stanislav@gmail.com>
2016-12-27 19:10:55 +0200

    left recursion wrote some lines of code

--- LL1Parse/left_recursion.py
+++ LL1Parse/left_recursion.py
@@ -1,1 +1,7 @@
 from left_factoring import *
+
+def left_recursion(nonterminal_list,production_list):
+    for production in production_list:
+        for left,right in production.items():
+
+left_recursion(input_dict["Grammar"]["Nonterminal"],input_dict["Grammar"]["Productions"])
